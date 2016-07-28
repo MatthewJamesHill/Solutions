6a376c07e19da5e1fdb08992495de2a8c91896df
Matthew Hill <matthewjameshill@live.com>
2016-07-28 22:22:32 +0100

    Add files via upload

---
+++ second.cs
@@ -0,0 +1,38 @@
+/*Method to search through string 
+and return a count of all lowercase characters */
+
+
+/*----------------------------------------SOLUTION----------------------------------------*/
+
+public class Program
+{
+    public static int LowercaseCountCheck(string str)
+    {
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsLower(str[i])) { count++; }
+        }
+
+        return count;
+    }
+}
+
+
+/*----------------------------------------REFACTOR 1----------------------------------------*/
+
+public class Refactor1
+{
+    public static int LowercaseCountCheck(string str)
+    {
+        int count = 0;
+
+        foreach (char c in str)
+        {
+            if (char.IsLower(c) { count++; }
+        }
+
+        return count;
+    }
+}
