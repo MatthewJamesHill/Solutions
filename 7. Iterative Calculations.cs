a68f602a83c42498bb24e4ad0a4138d6672ec2ca
Matthew Hill <matthewjameshill@live.com>
2016-08-07 09:52:43 +0100

    Add files via upload
    This solution became more verbose throughout the refactoring process due to the inefficiency of using the modulus operator. It was very challenging to optimise as the use of 10 was necessary throughout, but as it is not a power of 2, could not be straightforwardly translated into a bit shifting operation.

---
+++ 7. Iterative Calculations.cs
@@ -0,0 +1,129 @@
+﻿/* Find the sum of the digits of all the numbers from 1 to N (both ends included).
+
+For N = 10 the sum is 1+2+3+4+5+6+7+8+9+(1+0) = 46
+
+For N = 11 the sum is 1+2+3+4+5+6+7+8+9+(1+0)+(1+1) = 48
+
+For N = 12 the sum is 1+2+3+4+5+6+7+8+9+(1+0)+(1+1) +(1+2)= 51 */
+
+
+/*----------------------------------------REFACTOR 1----------------------------------------*/
+
+public class Solution
+{
+    public static long Solution(long n)
+    {
+        int Result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            int start = i;
+            int finish = 0;
+
+            while (start != 0)
+            {
+                finish += start % 10;
+                start /= 10;
+            }
+
+            Result += finish;
+        }
+        return Result;
+    }
+}
+
+
+/*----------------------------------------REFACTOR 1----------------------------------------*/
+
+public class Refactor1
+{
+    public static long Solution(long n)
+    {
+        int Result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            int start = i;
+
+            while (start != 0)
+            {
+                Result += start % 10;
+                start /= 10;
+            }
+        }
+        return Result;
+    }
+}
+
+
+/*----------------------------------------REFACTOR 2----------------------------------------*/
+
+public class Refactor2
+{
+    public static long Solution(long n)
+    {
+        int Result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int x = i; x != 0; x /= 10)
+            {
+                Result += x % 10;
+            }
+        }
+        return Result;
+    }
+}
+
+
+/*----------------------------------------REFACTOR 3----------------------------------------*/
+
+public class Refactor3
+{
+    public static long Solution(long n)
+    {
+        int Result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int x = i; x != 0; x /= 10)
+            {
+                Result += x - 10 * (x / 10);
+            }
+        }
+        return (long)Result;
+    }
+}
+
+
+/*----------------------------------------REFACTOR 4----------------------------------------*/
+
+public class Refactor4
+{
+    public static long Solution(long n)
+    {
+        int Result = 0;
+
+        if (n < 10)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Result += i;
+            }
+        }
+
+        else
+        {
+            Result = 45;
+
+            for (int i = 10; i <= n; i++)
+            {
+                for (int x = i; x != 0; x /= 10)
+                {
+                    Result += x - 10 * (x / 10);
+                }
+            }
+        }
+        return (long)Result;
+    }
+}
