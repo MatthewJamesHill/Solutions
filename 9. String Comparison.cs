39352e386819ccb00f4b663f8bb57ce643b4b96e
Matthew Hill <matthewjameshill@live.com>
2016-08-22 18:55:05 +0100

    Add files via upload

---
+++ 9. String Comparison.cs
@@ -0,0 +1,34 @@
+﻿/* Write a function that returns true if the characters of one string
+ * can be rearranged to make the word that is a second string */
+
+
+/*----------------------------------------SOLUTION----------------------------------------*/
+using System.Collections.Generic;
+
+
+public static class Solution
+{
+    public static bool Scramble(string str1, string str2)
+    {
+        List<char> letters = new List<char>();
+        foreach (char c in str1)
+        {
+            letters.Add(c);
+        }
+
+        foreach (char c in str2)
+        {
+            if (letters.Contains(c))
+            {
+                letters.Remove(c);
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
